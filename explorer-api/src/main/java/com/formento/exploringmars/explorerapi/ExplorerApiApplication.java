982a8ad5acb062868dc71431983874f3a70d80d0
André Formento <andreformento.sc@gmail.com>
2017-08-25 17:11:05 -0300

    Configure initial multi module project

--- explorer-api/src/main/java/com/formento/exploringmars/explorerapi/ExplorerApiApplication.java
+++ explorer-api/src/main/java/com/formento/exploringmars/explorerapi/ExplorerApiApplication.java
@@ -1,13 +1,13 @@
-package com.formento.exploringmars.explorermanager;
+package com.formento.exploringmars.explorerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExplorerManagerApplication {
+public class ExplorerApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ExplorerManagerApplication.class, args);
+        SpringApplication.run(ExplorerApiApplication.class, args);
     }
 
 }
--- explorer-api/src/test/java/com/formento/exploringmars/explorerapi/ExplorerApiApplicationTests.java
+++ explorer-api/src/test/java/com/formento/exploringmars/explorerapi/ExplorerApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.formento.exploringmars.explorermanager;
+package com.formento.exploringmars.explorerapi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ExplorerManagerApplicationTests {
+public class ExplorerApiApplicationTests {
 
     @Test
     public void contextLoads() {
