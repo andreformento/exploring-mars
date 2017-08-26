1b326e67e7c9f6f90c995622cb6bacf6600afb63
André Formento <andreformento.sc@gmail.com>
2017-08-26 17:44:53 -0300

    Implement RectangularPlanetArea

--- src/main/java/com/formento/exploringmars/model/impl/RectangularPlanetArea.java
+++ src/main/java/com/formento/exploringmars/model/impl/RectangularPlanetArea.java
@@ -1,9 +1,11 @@
 package com.formento.exploringmars.model.impl;
 
+import com.formento.exploringmars.infra.BusinessException;
 import com.formento.exploringmars.model.PlanetArea;
 import com.formento.exploringmars.model.Position;
 
 public class RectangularPlanetArea implements PlanetArea {
+
     private final Position leftBottom;
     private final Position rightTop;
 
@@ -14,7 +16,11 @@ public class RectangularPlanetArea implements PlanetArea {
 
     @Override
     public void validateBoundary(Position position) {
-
+        if (!position.isGreaterThanOrEqualTo(leftBottom)) {
+            throw new BusinessException("The point is less than left bottom");
+        } else if (!position.isLessThanOrEqualTo(rightTop)) {
+            throw new BusinessException("The point is greater than right top");
+        }
     }
 
 }
--- src/test/java/com/formento/exploringmars/model/impl/RectangularPlanetAreaTest.java
+++ src/test/java/com/formento/exploringmars/model/impl/RectangularPlanetAreaTest.java
@@ -1,17 +1,17 @@
 package com.formento.exploringmars.model.impl;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.only;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.formento.exploringmars.infra.BusinessException;
 import com.formento.exploringmars.model.Position;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -21,30 +21,32 @@ public class RectangularPlanetAreaTest {
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
-    @InjectMocks
     private RectangularPlanetArea planetArea;
     @Mock
     private Position leftBottom;
     @Mock
     private Position rightTop;
+    @Mock
+    private Position position;
+
+    @Before
+    public void init() {
+        this.planetArea = new RectangularPlanetArea(leftBottom, rightTop);
+    }
 
     @Test
     public void shouldBeValidWhenIsInsideOfBoundary() {
-        final Position position = mock(Position.class);
-
         when(position.isGreaterThanOrEqualTo(leftBottom)).thenReturn(true);
         when(position.isLessThanOrEqualTo(rightTop)).thenReturn(true);
 
         planetArea.validateBoundary(position);
 
-        verify(position, only()).isGreaterThanOrEqualTo(leftBottom);
-        verify(position, only()).isLessThanOrEqualTo(rightTop);
+        verify(position).isGreaterThanOrEqualTo(eq(leftBottom));
+        verify(position).isLessThanOrEqualTo(eq(rightTop));
     }
 
     @Test
     public void shouldNotBeValidWhenIsOutsideOfLeftBottomBoundary() {
-        final Position position = mock(Position.class);
-
         when(position.isGreaterThanOrEqualTo(leftBottom)).thenReturn(false);
         when(position.isLessThanOrEqualTo(rightTop)).thenReturn(true);
 
@@ -56,13 +58,11 @@ public class RectangularPlanetAreaTest {
 
     @Test
     public void shouldNotBeValidWhenIsOutsideOfRightBottomBoundary() {
-        final Position position = mock(Position.class);
-
         when(position.isGreaterThanOrEqualTo(leftBottom)).thenReturn(true);
         when(position.isLessThanOrEqualTo(rightTop)).thenReturn(false);
 
         expectedException.expect(BusinessException.class);
-        expectedException.expectMessage("The point is less than right top");
+        expectedException.expectMessage("The point is greater than right top");
 
         planetArea.validateBoundary(position);
     }
