01897e2465e046888e95aeb3c44470f1691bbd06
André Formento <andreformento.sc@gmail.com>
2017-08-27 12:50:27 -0300

    Organize imports

--- src/integration-test/java/com/formento/exploringmars/ExploringMarsApplicationTests.java
+++ src/integration-test/java/com/formento/exploringmars/ExploringMarsApplicationTests.java
@@ -1,6 +1,5 @@
 package com.formento.exploringmars;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
