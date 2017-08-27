079a5353eb08068020f5cd92e8e7a2debcfc5736
André Formento <andreformento.sc@gmail.com>
2017-08-27 13:05:29 -0300

    Add Deserialization test

--- src/integration-test/java/com/formento/exploringmars/model/impl/DriveCommandDefaultIT.java
+++ src/integration-test/java/com/formento/exploringmars/model/impl/DriveCommandDefaultIT.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,13 @@ public class DriveCommandDefaultIT {
         assertThat(objectMapper.writeValueAsString(DriveCommandDefault.MOVE)).isEqualTo("\"M\"");
         assertThat(objectMapper.writeValueAsString(DriveCommandDefault.TURN_LEFT)).isEqualTo("\"L\"");
         assertThat(objectMapper.writeValueAsString(DriveCommandDefault.TURN_RIGHT)).isEqualTo("\"R\"");
+    }
+
+    @Test
+    public void shouldDeserialize() throws IOException {
+        assertThat(objectMapper.readValue("\"M\"", DriveCommandDefault.class)).isEqualTo(DriveCommandDefault.MOVE);
+        assertThat(objectMapper.readValue("\"L\"", DriveCommandDefault.class)).isEqualTo(DriveCommandDefault.TURN_LEFT);
+        assertThat(objectMapper.readValue("\"R\"", DriveCommandDefault.class)).isEqualTo(DriveCommandDefault.TURN_RIGHT);
     }
 
 }
