b6ea1522bc3c471638361d88150e4d9b73a008a3
André Formento <andreformento.sc@gmail.com>
2017-08-28 00:09:42 -0300

    REmove MarsSpacialStationControllerBDD

--- src/integration-test/java/com/formento/exploringmars/api/v1/controller/MarsSpacialStationControllerIT.java
+++ src/integration-test/java/com/formento/exploringmars/api/v1/controller/MarsSpacialStationControllerIT.java
@@ -3,16 +3,22 @@ package com.formento.exploringmars.api.v1.controller;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
+import io.restassured.http.ContentType;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.restassured.module.mockmvc.specification.MockMvcRequestSpecification;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
@@ -20,20 +26,30 @@ import org.springframework.web.context.WebApplicationContext;
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class MarsSpacialStationControllerIT {
 
+    private static final String PATH = "/spacial-station";
+
     @Autowired
     private WebApplicationContext context;
-    private MarsSpacialStationControllerBDD bdd;
+
+    private MockMvcRequestSpecification given;
 
     @Before
     public void init() {
-        this.bdd = new MarsSpacialStationControllerBDD(context);
+        final MockMvc mvc = webAppContextSetup(context).build();
+        this.given = RestAssuredMockMvc.given().mockMvc(mvc).contentType(MediaType.APPLICATION_JSON_VALUE).accept(ContentType.JSON);
     }
 
     @Test
     public void shouldNavigateOnMars() {
-        bdd.
-            givenSimpleBody().
-            whenExplorePlanet().
+        given.
+            body("{\n"
+                + "    \"x\": 1,\n"
+                + "    \"y\": 2,\n"
+                + "    \"navigationSense\": \"N\",\n"
+                + "    \"driveCommands\": [\"M\",\"L\",\"R\",\"R\"]\n"
+                + "}").
+            post(PATH + "/explore-planet").
+            then().
             assertThat().
             statusCode(is(HttpStatus.OK.value())).
             body("x", equalTo(1)).
@@ -45,14 +61,15 @@ public class MarsSpacialStationControllerIT {
     public void shouldNavigateOnMarsAfterAnotherGroundProbe() {
         shouldNavigateOnMars();
 
-        bdd.
-            givenBody("{\n"
+        given.
+            body("{\n"
                 + "    \"x\": 1,\n"
                 + "    \"y\": 2,\n"
                 + "    \"navigationSense\": \"W\",\n"
                 + "    \"driveCommands\": [\"M\",\"R\"]\n"
                 + "}").
-            whenExplorePlanet().
+            post(PATH + "/explore-planet").
+            then().
             assertThat().
             statusCode(is(HttpStatus.OK.value())).
             body("x", equalTo(0)).
@@ -62,37 +79,38 @@ public class MarsSpacialStationControllerIT {
 
     @Test
     public void shouldNavigateOnMarsWithMultiMoviments() {
-        bdd.
-            givenBody("{\n"
+        given.
+            body("{\n"
                 + "    \"x\": 1,\n"
                 + "    \"y\": 2,\n"
                 + "    \"navigationSense\": \"N\",\n"
                 + "    \"driveCommands\": [\"L\",\"M\",\"L\",\"M\",\"L\",\"M\",\"L\",\"M\",\"M\"]\n"
                 + "}").
-            whenExplorePlanet().
+            post(PATH + "/explore-planet").
+            then().
             assertThat().
             statusCode(is(HttpStatus.OK.value())).
             body("x", equalTo(1)).
             body("y", equalTo(3)).
             body("navigationSense", equalTo("N"));
 
-        bdd.
-            givenBody("{\n"
+        given.
+            body("{\n"
                 + "    \"x\": 3,\n"
                 + "    \"y\": 3,\n"
                 + "    \"navigationSense\": \"E\",\n"
                 + "    \"driveCommands\": [\"M\",\"M\",\"R\",\"M\",\"M\",\"R\",\"M\",\"R\",\"R\",\"M\"]\n"
                 + "}").
-            whenExplorePlanet().
+            post(PATH + "/explore-planet").
+            then().
             assertThat().
             statusCode(is(HttpStatus.OK.value())).
             body("x", equalTo(5)).
             body("y", equalTo(1)).
             body("navigationSense", equalTo("E"));
 
-        bdd.
-            given().
-            whenDoGet().
+        given.
+            get(PATH).then().
             assertThat().
             statusCode(is(HttpStatus.OK.value())).
             body("size()", is(2)).
@@ -108,9 +126,15 @@ public class MarsSpacialStationControllerIT {
     public void shouldNotNavigateInTheSamePosition() {
         shouldNavigateOnMars();
 
-        bdd.
-            givenSimpleBody().
-            whenExplorePlanet().
+        given.
+            body("{\n"
+                + "    \"x\": 1,\n"
+                + "    \"y\": 2,\n"
+                + "    \"navigationSense\": \"N\",\n"
+                + "    \"driveCommands\": [\"M\",\"L\",\"R\",\"R\"]\n"
+                + "}").
+            post(PATH + "/explore-planet").
+            then().
             assertThat().
             statusCode(is(HttpStatus.BAD_REQUEST.value())).
             body("message", equalTo("The position (1, 3) is busy"));
@@ -120,9 +144,8 @@ public class MarsSpacialStationControllerIT {
     public void shouldReportGroundProbe() {
         shouldNavigateOnMarsAfterAnotherGroundProbe();
 
-        bdd.
-            given().
-            whenDoGet().
+        given.
+            get(PATH).then().
             statusCode(is(HttpStatus.OK.value())).
             body("size()", is(2)).
             body("x", hasItem(1)).
@@ -135,13 +158,14 @@ public class MarsSpacialStationControllerIT {
 
     @Test
     public void shouldDeployGroundProbeOnPlanet() {
-        bdd.
-            givenBody("{\n"
+        given.
+            body("{\n"
                 + "    \"x\": 3,\n"
                 + "    \"y\": 4,\n"
                 + "    \"navigationSense\": \"S\"\n"
                 + "}").
-            whenDeployGroundProbeOnPlanet().
+            post(PATH).
+            then().
             assertThat().
             statusCode(is(HttpStatus.CREATED.value())).
             body("x", equalTo(3)).
@@ -152,9 +176,10 @@ public class MarsSpacialStationControllerIT {
     @Test
     public void shouldExplorePlanetWithCreatedGroundProbe() {
         shouldNavigateOnMars();
-        bdd.
-            givenBody("[\"L\",\"M\",\"M\",\"L\"]").
-            whenExplorePlanet(1, 3). // E
+        given.
+            body("[\"L\",\"M\",\"M\",\"L\"]").
+            put(PATH + "/explore-planet/" + 1 + "/" + 3). // E
+            then().
             assertThat().
             statusCode(is(HttpStatus.OK.value())).
             body("x", equalTo(1)).
@@ -164,9 +189,10 @@ public class MarsSpacialStationControllerIT {
 
     @Test
     public void shouldNotExplorePlanetGroundProbeNotExistsOnPosition() {
-        bdd.
-            givenBody("[\"L\"]").
-            whenExplorePlanet(1, 3). // EassertThat().
+        given.
+            body("[\"L\"]").
+            put(PATH + "/explore-planet/" + 1 + "/" + 3). // E
+            then().
             statusCode(is(HttpStatus.NOT_FOUND.value())).
             body("message", equalTo("Ground probe not found at position (1, 3)"));
     }
